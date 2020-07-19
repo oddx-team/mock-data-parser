bcfa93120a827227c74c14f50e270be35dca0e68
Lam Ngo <ngonl@garena.com>
2020-07-20 00:13:06 +0800

    UPD: Reformat code

--- utils.js
+++ utils.js
@@ -1,38 +1,38 @@
-const fs = require("fs");
-const resultFolder = "./result";
+const fs = require('fs');
+const resultFolder = './result';
 const MAX_LENGTH = 77;
 
 const writeToFile = async (fileName, data) => {
-	fs.writeFile(
-		`${resultFolder}/${fileName}`,
-		JSON.stringify(data, null, 2),
-		err => console.log(err)
-	);
+  fs.writeFile(`${resultFolder}/${fileName}`, JSON.stringify(data, null, 2), (err) =>
+    console.log(err),
+  );
 };
 
 const initResultFolder = () => {
-	if (!fs.existsSync(resultFolder)) {
-		fs.mkdirSync(resultFolder);
-	}
+  if (!fs.existsSync(resultFolder)) {
+    fs.mkdirSync(resultFolder);
+  }
 };
 
 const processRawCards = (data, color, language) => {
   let result = [];
-	Object.values(data).slice(0, 100).map((item, _) => {
-    if (item.text.length <= MAX_LENGTH) {
-      result.push({
-				color, 
-				language,
-        text: item.text,
-        ...(item.pick && item.pick <= 2 && {gaps: item.pick}),
-      });
-    }
-	});
-	return result;
+  Object.values(data)
+    .slice(0, 100)
+    .map((item, _) => {
+      if (item.text.length <= MAX_LENGTH) {
+        result.push({
+          color,
+          language,
+          text: item.text,
+          ...(item.pick && item.pick <= 2 && { gaps: item.pick }),
+        });
+      }
+    });
+  return result;
 };
 
 module.exports = {
-	writeToFile,
+  writeToFile,
   initResultFolder,
-  processRawCards
+  processRawCards,
 };
